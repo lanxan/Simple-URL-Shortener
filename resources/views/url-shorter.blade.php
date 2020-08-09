2e7ed4186a342566f0b608972d232c4d97df5c78
李翌辰 <lanxan0309@gmail.com>
2020-08-09 23:22:05 +0800

    first commit

---
+++ resources/views/url-shorter.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Url Shorter!!!</title>
+    </head>
+    <body>
+        <form>
+            <p>
+                Url: <input style="width:100%" />
+            </p>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
---
+++ storage/framework/views/86667f1a1e935c525bc37175d977f9d5e9be5a2d.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Url Shorter!!!</title>
+    </head>
+    <body>
+        <form>
+            <p>
+                Url: <input style="width:100%" />
+            </p>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
+<?php /**PATH /Users/lanxan0309/Projects/url-shorter/resources/views/url-shorter.blade.php ENDPATH**/ ?>
